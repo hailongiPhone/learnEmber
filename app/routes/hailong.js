5caf99d34f40ca15dbf1802b67b2a58d204858dc
hailong <ghl_hh@hotmail.com>
2017-04-04 22:08:37 +0900

    - update 在route 的hook方法model方法中创建数据 Promise

--- app/routes/hailong.js
+++ app/routes/hailong.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(){
+    return [
+      {
+        id:1,
+        location: 'Asilomar',
+        sightedAt: new Date('2016-03-07')
+      },
+      {
+        id:2,
+        location: 'Asilomar',
+        sightedAt: new Date('2016-03-07')
+      }
+    ];
+  }
 });
