bd4eb80ee1b55bb9dbe9df804fcfa9c0bf7dfa1c
hailong <ghl_hh@hotmail.com>
2017-04-05 02:39:10 +0900

    - new sighting

---
+++ app/controllers/sightings/new.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  actions: {
+    create(){
+      var self = this;
+      this.get('model.sighting').save().then(function () {
+        self.transitionToRoute('sightings');
+      });
+    },
+    cancel(){
+
+    }
+  }
+});
--- app/routes/sightings/new.js
+++ app/routes/sightings/new.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(){
+    return Ember.RSVP.hash({
+        sighting: this.store.createRecord('sighting'),
+        cryptids: this.store.findAll('cryptid'),
+        witnesses: this.store.findAll('witness')
+    });
+  }
 });
